381cdb127b0492e67569b59a169ae7efe7901ab2
Rahul kardile <rahulkardile321@gmail.com>
2024-07-26 16:23:09 +0530

    rockstar start

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+try {
+    mongoose.connect(process.env.MONGO_url).then(() => {
+        console.log("database is connected");
+    })
+} catch (error) {
+    console.log("database is not connected");
+}
+
+const PORT = process.env.PORT || 3300;
+
+app.get("/", (req, res) => {
+    const data = req.body.data;
+
+    res.status(200).json({
+        success: true,
+        message: `data is ${data}`
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server Running on ${PORT}`);
+});
+
